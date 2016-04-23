88637c62ec0b5771bdcd2e5e98920f8c45eaa5c3
joaosr <eng.jmsoares@gmail.com>
2016-04-23 19:13:14 -0300

    Return layout orange

--- nairalirayoga/settings.py
+++ nairalirayoga/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'test_without_migrations',
     'nairalirayoga.core',
+    'storages',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -118,11 +119,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
